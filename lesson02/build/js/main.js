afc8f4ed7005cf018a8489a3fc020202e79c1935
maciek <maciek@lenovo.mc78.local>
2022-11-22 12:17:25 +0100

    lesson02 complete

---
+++ lesson02/build/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+let myName = "Maciek";
+let meaningOfLive;
+let isLoading;
+let album;
+myName = "Bartuś";
+meaningOfLive = 44;
+isLoading = true;
+album = 44;
+album = "44";
+const sum = (a, b) => {
+    return a + b;
+};
+let postid;
+let isActive;
+let re = /\w+/g;
---
+++ lesson02/src/main.ts
@@ -0,0 +1,19 @@
+let myName: string = "Maciek";
+let meaningOfLive: number;
+let isLoading: boolean;
+let album: any;
+
+myName = "Bartuś";
+meaningOfLive = 44;
+isLoading = true;
+album = 44;
+album = "44";
+
+const sum = (a: number, b: string) => {
+  return a + b;
+};
+
+let postid: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g;
